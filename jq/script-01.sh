39b8894ab715041a293d96c7c18a85df0efad21a
miriamjorna <hoimirextra@gmail.com>
2026-08-01 00:03:43 +0100

    taking jq out of this branch

--- jq/script-01.sh
+++ jq/script-01.sh
@@ -5,4 +5,3 @@ set -euo pipefail
 # The input for this script is the person.json file.
 # TODO: Write a command to output the name of the person.
 # Your output should be exactly the string "Selma", but should not contain any quote characters.
-jq -r .name ./person.json
--- jq/script-03.sh
+++ jq/script-03.sh
@@ -5,4 +5,3 @@ set -euo pipefail
 # The input for this script is the person.json file.
 # TODO: Write a command to output the name of the person, then a comma, then their profession.
 # Your output should be exactly the string "Selma, Software Engineer", but should not contain any quote characters.
-jq -r '[.name, .profession] | join(", ")' ./person.json
